9b18da6cee7ddb655fd4b3731eb57378428a0c31
Mai Eaksen <eaksens@mail.gvsu.edu>
2019-04-19 21:27:21 -0400

    4th commit

--- calendar1.js
+++ calendar1.js
@@ -7,9 +7,9 @@
  */
 var i, j;
 var date;
+var month, year;
 
-function initializeCalendar()
-{
+function initializeCalendar() {
   date = new Date();
 }
 
@@ -20,91 +20,109 @@ function displayCalendar() {
 }
 
 //will populate calendar with days, 1-31, depending on a month
-function fillDays(){
+function fillDays() {
   var i;
   var calendarDays = document
-  .getElementById("calendarTable")
-  .getElementsByTagName("tbody")[0]
-  .getElementsByClassName("day");
+    .getElementById("calendarTable")
+    .getElementsByTagName("tbody")[0]
+    .getElementsByClassName("day");
 
-  var month = date.getMonth();
-  var year = date.getFullYear();
-  var daysOffset = new Date(year,month,1).getDay();
-  var numberOfDays = new Date(year,month+1,0).getDate(); //0 for day gives last day of the PREVIOUS month
+  //get month -current month is April -so this got 3
+  month = date.getMonth();
+  console.log(month);
+  //get year -current year is 2019
+  year = date.getFullYear();
+  console.log(year);
+  var daysOffset = new Date(year, month, 1).getDay();
+  console.log(daysOffset);
+  var numberOfDays = new Date(year, month + 1, 0).getDate(); //0 for day gives last day of the PREVIOUS month
+  console.log(numberOfDays);
 
   //clear cells
-  for(i=0; i<calendarDays.length;i++){
+  for (i = 0; i < calendarDays.length; i++) {
     calendarDays[i].innerHTML = "&nbsp";
   }
 
   //populate cells with dates, 1-31
-  for(i=1; i<=numberOfDays; i++)
-  {
+  for (i = 1; i <= numberOfDays; i++) {
     calendarDays[daysOffset].innerHTML = i;
     daysOffset++;
   }
 }
 
-function getPreviousMonth(){
+function getPreviousMonth() {
   var day = date.getDate();
   var month = date.getMonth();
   var year = date.getFullYear();
-  if(month == 0){
+  if (month == 0) {
     month = 11;
     year--;
   } else {
     month--;
   }
-  date = new Date(year,month,day);
+  date = new Date(year, month, day);
 
   return date;
 }
 
-function getNextMonth(){
+function getNextMonth() {
   var day = date.getDate();
   var month = date.getMonth();
   var year = date.getFullYear();
-  if(month == 11){
+  if (month == 11) {
     month = 0;
     year++;
   } else {
     month++;
   }
-  date = new Date(year,month,day);
+  date = new Date(year, month, day);
 
   return date;
 }
 
-function setMonthTitle(){
+function setMonthYearTitle() {
   //update year header on a table
+  fillDays();
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  switch (month) {
+    case 0:
+      document.getElementById("pageTile").innerHTML = months[0] + " " + year;
+      break;
+    case 1:
+      document.getElementById("pageTile").innerHTML = months[1] + " " + year;
+      break;
+    case 2:
+      document.getElementById("pageTile").innerHTML = months[2] + " " + year;
+      break;
+    case 3:
+      document.getElementById("pageTile").innerHTML = months[3] + " " + year;
+      break;
+    case 4:
+      document.getElementById("pageTile").innerHTML = months[4] + " " + year;
+      break;
+    case 5:
+      document.getElementById("pageTile").innerHTML = months[5] + " " + year;
+      break;
+    case 6:
+      document.getElementById("pageTile").innerHTML = months[6] + " " + year;
+      break;
+    case 7:
+      document.getElementById("pageTile").innerHTML = months[7] + " " + year;
+      break;
+    case 8:
+      document.getElementById("pageTile").innerHTML = months[8] + " " + year;
+      break;
+    case 9:
+      document.getElementById("pageTile").innerHTML = months[9] + " " + year;
+      break;
+    case 10:
+      document.getElementById("pageTile").innerHTML = months[10] + " " + year;
+      break;
+    case 11:
+      document.getElementById("pageTile").innerHTML = months[11] + " " + year;
+      break;
+  }
 }
-
-function setYearTitle(){
-  //update month header on a table
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -133,7 +151,7 @@ var update = function(date) {
 
 
 
-function nextMonth(){
+function nextMonth() {
   getNextMonth();
   displayCalendar();
 
